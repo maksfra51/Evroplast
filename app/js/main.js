ee2d49d7a4fe0c7f1f69c319a7be9481e2679b33
maksfra51 <stanislav.poltavets@mail.ru>
2021-02-07 12:52:27 +0300

    added popup

--- app/js/main.js
+++ app/js/main.js
@@ -17,8 +17,8 @@ $(function (){
     prevArrow: '<button type="button" class="slick-prev"><svg width="17" height="30" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 1L2 15L16 29" stroke="#E2E2E2" stroke- width="2" stroke - linecap="round" /></svg ></button>',
     nextArrow: '<button type="button" class="slick-next"><svg width="17" height="30" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 29L15 15L1 0.999998" stroke="#E2E2E2" stroke- width="2" stroke - linecap="round" /></svg ></button>',
   });
-  $('.tabs__btn ').on('click', function () {
-    $('.tabs__btn ').removeClass('tabs__btn--active');
+  $('.tabs__btn').on('click', function () {
+    $('.tabs__btn').removeClass('tabs__btn--active');
     $(this).addClass('tabs__btn--active');
   });
   var mixer = mixitup('main',{
@@ -26,6 +26,26 @@ $(function (){
       enable: false,
     }
   });
+
+  $('.header__top-feedback').on('click', function () {
+    $('.popup').addClass('open');
+  });
+
+  $('.popup__title').on('click', function () {
+    $('.popup').removeClass('open');
+  });
+
+
+
+
+  $('.page__menu-btn').on('click', function () {
+    $('.popup-price').addClass('open');
+  });
+
+  $('.popup-price__title').on('click', function () {
+    $('.popup-price').removeClass('open');
+  });
+
 });
 
 
